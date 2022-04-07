f48f6ddffd34cdd87d15c3983c408bc3a2da044f
PaulS <psoliz333@gmail.com>
2022-04-07 02:08:36 -0500

    added player health and reduced health affects

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,7 +2,7 @@ const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
 
-console.log(new Potion)
+//console.log(new Potion)
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -13,4 +13,33 @@ test('creates a player object', () => {
     expect(player.agility).toEqual(expect.any(Number));
 
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
-})
+});
+
+test("gets player's health value", () => {
+    const player = new Player ('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player ('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player ('Dave')
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5)
+
+    player.reduceHealth(9999);
+
+    expect(player.health).toBe(0);
+});
