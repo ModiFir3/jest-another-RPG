d0187fae0f313005b6d49e6b6c49e89da953edc8
PaulS <psoliz333@gmail.com>
2022-04-08 02:10:36 -0500

    added inital game function and started battle functions

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -69,4 +69,23 @@ test('uses a potion from inventory', () => {
     player.usePotion(1);
 
     expect(player.inventory.length).toBeLessThan(oldCount);
-})
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
